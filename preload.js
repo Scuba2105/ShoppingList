4bd180d074399d30cf8e63ef7a7529d0b53d154d
Steven Bradbury <scuba2150@gmail.com>
2023-05-22 11:29:16 +1000

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
   sendWeeklyData: () => ipcRenderer.invoke('data:sendWeeklyData'),
   saveData: (shoppingList) => ipcRenderer.send('data:saveData', shoppingList),
   listenForSave: () => ipcRenderer.on('save-data-success', (evt, message) => {
-    alert(message); 
+    messageText.textContent = message;
+    messageBox.style.display = 'flex'; 
   })   
 });
